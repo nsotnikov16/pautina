a4751975e1f27d4d86d3946c98abbbe41194c97f
Nikita Sotnikov <nsotnikov16@yandex.ru>
2022-05-25 01:03:26 +0500

    update

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,3 +1,5 @@
+
+
 /* Спойлеры */
 const spoilers = document.querySelectorAll('.spoiler')
 spoilers.forEach(spoiler => {
@@ -7,12 +9,81 @@ spoilers.forEach(spoiler => {
     })
 })
 
+/* Фотогаллерея */
 var gallery = new Swiper(".gallery", {
-    slidesPerView: 4,
+    loop: true,
     centeredSlides: true,
     grabCursor: true,
-    spaceBetween: 100
-    
+    spaceBetween: 100,
+    navigation: {
+        nextEl: ".gallery .swiper-button-next",
+        prevEl: ".gallery .swiper-button-prev",
+    },
+    breakpoints: {
+        1280: {
+            slidesPerView: 2
+        }
+    }
 });
-gallery.allowSlideNext = true
-gallery.activeIndex = 2
+
+
+/* Карточки */
+var cards = new Swiper('.cards', {
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+    },
+    breakpoints: {
+        1280: {
+            spaceBetween: 30,
+            slidesPerView: 4,
+        },
+
+        820: {
+            spaceBetween: 30,
+            slidesPerView: 3,
+        },
+        480: {
+            spaceBetween: 20,
+            slidesPerView: 2,
+        },
+        320: {
+            spaceBetween: 20,
+            slidesPerView: 'auto',
+        }
+    }
+})
+
+/* Показать все */
+const btns = document.querySelectorAll('.btn_all')
+btns.forEach(btn => {
+    const section = btn.closest('section')
+    const items = section.querySelectorAll('.services__item')
+
+    function display(value) {
+        items.forEach((item, ind) => ind > 2 ? item.style.display = value : '')
+    }
+
+    if (items.length > 3) {
+        display('none')
+        btn.addEventListener('click', () => {
+            display('block')
+            btn.remove()
+        })
+    } else {
+        btn.remove()
+    }
+})
+
+/* Позвонить на мобильнике */
+const isMobile = /Mobile|webOS|BlackBerry|IEMobile|MeeGo|mini|Fennec|Windows Phone|Android|iP(ad|od|hone)/i.test(navigator.userAgent);
+if (isMobile) {
+    const mobileBottom = document.querySelector('.mobile')
+    if (mobileBottom) {
+        window.addEventListener('scroll', () => {
+            const scroll = Math.ceil(window.scrollY)
+            if (scroll >= 400) mobileBottom.classList.add('show')
+            if (scroll < 400) mobileBottom.classList.remove('show')
+        })
+    }
+}
