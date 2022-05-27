ea0afaba9920e61b0869b76c16b331b2daae4091
Nikita Sotnikov <nsotnikov16@yandex.ru>
2022-05-27 14:34:06 +0500

    update

--- dist/js/app.js
+++ dist/js/app.js
@@ -244,3 +244,47 @@ var swiper = new Swiper(".detail-swiper", {
     },
     mousewheel: true,
 });
+
+/* Анимация при скролле */
+
+let titles = [...document.querySelectorAll('.section__title'), ... document.querySelectorAll('.section__subtitle')]
+titles.push(document.querySelector('.banner__title'))
+titles.forEach(item => item.classList.add('_anim-items', '_anim-no-hide'))
+
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll);
+    function animOnScroll() {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                animItem.classList.add('_animate');
+            } else {
+                if (!animItem.classList.contains('_anim-no-hide')) {
+                    animItem.classList.remove('_animate');
+                }
+            }
+        }
+    }
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+    }
+
+    setTimeout(() => {
+        animOnScroll();
+    }, 10);
+}
+
